b613d4e83a577dfac744b9b8201b739fd1c2f079
Kirill Alekseev <kirill.alekseev1613@gmail.com>
2021-06-02 13:58:35 +0300

    commit

---
+++ src/main/java/tech/itpark/project_delivery_web/security/jwt/TokenAuthenticationFilter.java
@@ -0,0 +1,53 @@
+package tech.itpark.project_delivery_web.security.jwt;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TokenAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    public TokenAuthenticationFilter() {
+        super("/**");
+        setAuthenticationSuccessHandler((request, response, authentication) ->
+        {
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            request.getRequestDispatcher(request.getServletPath() + request.getPathInfo()).forward(request, response);
+        });
+        setAuthenticationFailureHandler((request, response, authenticationException) -> {
+            response.getOutputStream().print(authenticationException.getMessage());
+        });
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException, IOException, ServletException {
+        String token = request.getHeader("token");
+        if (token == null)
+            token = request.getParameter("token");
+        if (token == null) {
+
+//            authentication.setAuthenticated(false);
+//            return authentication;
+            return null;
+        }
+//        TokenAuthentication tokenAuthentication = new TokenAuthentication(token);
+//        Authentication authentication = getAuthenticationManager().authenticate(tokenAuthentication);
+//        return authentication;
+        return null;
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res,
+                         FilterChain chain) throws IOException, ServletException {
+        super.doFilter(req, res, chain);
+    }
+}
